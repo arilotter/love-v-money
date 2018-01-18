11bafa576f911e3e338f068ec48f3d62a033617a
Ari Lotter <arilotter@gmail.com>
2018-01-18 17:41:17 -0500

    analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Analytics from "react-router-ga";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Analytics id="UA-38748116-2">
+      <App />
+    </Analytics>
   </BrowserRouter>,
   document.getElementById("root")
 );
